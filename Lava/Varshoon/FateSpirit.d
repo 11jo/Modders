47fa85cf2c48ba9245152fc8a0e466b8f821eef8
11jo <79876551+11jo@users.noreply.github.com>
2026-08-24 00:32:47 +0200

    Wrong DeathVar

--- Lava/Varshoon/FateSpirit.d
+++ Lava/Varshoon/FateSpirit.d
@@ -1,9 +1,9 @@
 //////////////////////FateSpirit
 
 EXTEND_BOTTOM FATESP 6 #4
-IF ~ !Dead("VxVar") !InMyArea("VxVar") CheckStatGT(Player1,14,INT) Global("VxVarSummonedToB","GLOBAL",0)~ THEN REPLY @0 DO ~CreateVisualEffect("SPPORTAL",[1999.1228])
+IF ~ !Dead("VARSHOON") !InMyArea("VARSHOON") CheckStatGT(Player1,14,INT) Global("VxVarSummonedToB","GLOBAL",0)~ THEN REPLY @0 DO ~CreateVisualEffect("SPPORTAL",[1999.1228])
 Wait(2) 
 CreateCreature("VxVar25",[1999.1228],0) 
 SetGlobal("VxVarSummonedToB","GLOBAL",1)
-ActionOverride("VxVar",StartDialogueNoSet(Player1))~ GOTO 8
+ActionOverride("VARSHOON",StartDialogueNoSet(Player1))~ GOTO 8
 END
